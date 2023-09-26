d845cfe9ebb5bf8be83da3c79db7b248e53c37ca
sirsa4 <yaya.abbdi@gmail.com>
2023-09-26 14:21:42 +0200

    css fully done

--- src/app/_components/Articles.tsx
+++ src/app/_components/Articles.tsx
@@ -7,65 +7,65 @@ import CompleteBtn from "./CompleteBtn"
 const Articles = () => {
   return (
     <main className="flex flex-col">
-      <h2>My Todos</h2>
-      <section className="flex flex-col flex-grow md:grid md:grid-cols-3 gap-4">
-        <article className="flex w-full flex-col flex-grow gap-1 p-4 text-center shadow-md ">
-          <h3>Todotitle</h3>
-          <p>
+      <h2 className="font-black">My Todos</h2>
+      <section className="flex flex-col flex-grow md:grid md:grid-cols-3 gap-4 p-10">
+        <article className="flex w-full flex-col flex-grow gap-4 p-4 text-center shadow-md">
+          <h3 className="md:text-start font-bold">Todotitle</h3>
+          <p className="text-start">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
             harum quas qui! Ipsa, nihil vero.
           </p>
-          <div className="btn-container">
+          <div className="flex justify-center md:justify-end">
             <CompleteBtn />
           </div>
         </article>
-        <article className="flex w-full flex-col flex-grow gap-1 p-4 text-center shadow-md">
-          <h3>Todotitle</h3>
-          <p>
+        <article className="flex w-full flex-col flex-grow gap-4 p-4 text-center shadow-md">
+          <h3 className="md:text-start font-bold">Todotitle</h3>
+          <p className="text-start">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
             harum quas qui! Ipsa, nihil vero.
           </p>
-          <div className="btn-container">
+          <div className="flex justify-center md:justify-end">
             <CompleteBtn />
           </div>
         </article>
-        <article className="flex w-full flex-col flex-grow gap-1  p-4 text-center shadow-md">
-          <h3>Todotitle</h3>
-          <p>
+        <article className="flex w-full flex-col flex-grow gap-4 p-4 text-center shadow-md">
+          <h3 className="md:text-start font-bold">Todotitle</h3>
+          <p className="text-start">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
             harum quas qui! Ipsa, nihil vero.
           </p>
-          <div className="btn-container">
+          <div className="flex justify-center md:justify-end">
             <CompleteBtn />
           </div>
         </article>
-        <article className="flex w-full flex-col flex-grow gap-1 p-4 text-center shadow-md">
-          <h3>Todotitle</h3>
-          <p>
+        <article className="flex w-full flex-col flex-grow gap-4 p-4 text-center shadow-md">
+          <h3 className="md:text-start font-bold">Todotitle</h3>
+          <p className="text-start">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
             harum quas qui! Ipsa, nihil vero.
           </p>
-          <div className="btn-container">
+          <div className="flex justify-center md:justify-end">
             <CompleteBtn />
           </div>
         </article>
-        <article className="flex w-full flex-col flex-grow gap-1 p-4 text-center shadow-md">
-          <h3>Todotitle</h3>
-          <p>
+        <article className="flex w-full flex-col flex-grow gap-4 p-4 text-center shadow-md">
+          <h3 className="md:text-start font-bold">Todotitle</h3>
+          <p className="text-start">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
             harum quas qui! Ipsa, nihil vero.
           </p>
-          <div className="btn-container">
+          <div className="flex justify-center md:justify-end">
             <CompleteBtn />
           </div>
         </article>
-        <article className="flex w-full flex-col flex-grow gap-1 p-4 text-center shadow-md">
-          <h3>Todotitle</h3>
-          <p>
+        <article className="flex w-full flex-col flex-grow gap-4 p-4 text-center shadow-md">
+          <h3 className="md:text-start font-bold">Todotitle</h3>
+          <p className="text-start">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
             harum quas qui! Ipsa, nihil vero.
           </p>
-          <div className="btn-container">
+          <div className="flex justify-center md:justify-end">
             <CompleteBtn />
           </div>
         </article>

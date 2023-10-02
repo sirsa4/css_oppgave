fda09ce018ffff8a7c402041bacfada602dc747c
sirsa4 <yaya.abbdi@gmail.com>
2023-10-02 22:09:19 +0200

    error alert added

--- src/context/TodoContext.tsx
+++ src/context/TodoContext.tsx
@@ -90,6 +90,7 @@ const TodoContext = ({ children }: { children: ReactNode }) => {
     setTitle("")
     setText("")
     }
+    alert("Write a title and text pls")
   }
 
   const handleComplete = (currentID: number): void => {
@@ -98,42 +99,6 @@ const TodoContext = ({ children }: { children: ReactNode }) => {
 
   return (
     <>
-      {/* {todos
-        ? todos.map((todo: TodoType) => {
-            return (
-              <div key={todo.id}>
-                <h3>id: {todo.id}</h3>
-                <p>{todo.title}</p>
-                <p>{todo.text}</p>
-                {todo.isComplete ? (
-                  <p>Completed</p>
-                ) : (
-                  <button type="button" onClick={() => handleComplete(todo.id)}>
-                    Complete
-                  </button>
-                )}
-              </div>
-            )
-          })
-        : "nothing"}
-
-      <input
-        className="m-4 block border border-black"
-        value={title}
-        onChange={getTitle}
-      />
-      <textarea
-        name="text"
-        id=""
-        cols={10}
-        rows={2}
-        className="border border-black"
-        value={text}
-        onChange={getText}
-      ></textarea>
-      <button type="button" onClick={handleAddTodo}>
-        Add todo
-      </button> */}
       <TodoProvider.Provider value={{ todos, todosDispatch, title,setTitle,text,setText,
       getTitle,getText,handleAddTodo,handleComplete
       }}>
